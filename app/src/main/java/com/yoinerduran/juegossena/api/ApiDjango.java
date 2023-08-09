2f1e3a3011b076c40fa209afd58e83e23e258d30
Yoiner Duran <duranyoiner@gmail.com>
2023-08-09 12:01:57 -0500

    dispositivos

--- app/src/main/java/com/yoinerduran/juegossena/api/ApiDjango.java
+++ app/src/main/java/com/yoinerduran/juegossena/api/ApiDjango.java
@@ -4,12 +4,12 @@ import android.content.Context;
 
 public class ApiDjango {
     //local
-    static public String url="http://192.168.110.39:8000";
-    static public String urlMedia="http://192.168.110.39:8000";
+    //static public String url="http://192.168.110.39:8000";
+    //static public String urlMedia="http://192.168.110.39:8000";
 
     //web
-    //static public String url="https://juegossena.azurewebsites.net";
-    //static public String urlMedia="";
+    static public String url="https://juegossena.azurewebsites.net";
+    static public String urlMedia="";
 
     static String urlApi=url+"/api/";
     static public String urlInicioSesion(Context context){
